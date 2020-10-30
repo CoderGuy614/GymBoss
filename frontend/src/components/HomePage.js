2341b2ed7f6480821708b06ed01f627ac77abebb
CoderGuy614 <jlutz.110@gmail.com>
2020-10-30 14:36:05 -0400

    Add Formatting and Content to Home Page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -5,18 +5,21 @@ const HomePage = () => {
   const plans = [
     {
       title: "Bronze Plan",
+      price: "25",
       description: "Bronze Plan Description",
       color: " bronze",
       url: "/bronze",
     },
     {
       title: "Silver Plan",
+      price: "35",
       description: "Silver Plan Description",
       color: " silver",
       url: "/silver",
     },
     {
       title: "Gold Plan",
+      price: "45",
       description: "Gold Plan Description",
       color: " gold",
       url: "/gold",
@@ -24,19 +27,32 @@ const HomePage = () => {
   ];
 
   return (
-    <>
+    <Container className="justify-content-center">
+      <Row className="justify-content-center">
+        <h1>Choose a Plan</h1>
+      </Row>
+      <hr />
       <Row>
         {plans.map((p, i) => (
           <PlanCard
             key={i}
             title={p.title}
+            price={p.price}
             description={p.description}
             color={p.color}
             url={p.url}
           />
         ))}
       </Row>
-    </>
+      <hr />
+      <Row className="justify-content-center">
+        <h1>Already a Member ?</h1>
+      </Row>
+      <hr />
+      <Button size="lg" block variant="outline-primary" href="/login">
+        Sign In
+      </Button>
+    </Container>
   );
 };
 
--- frontend/src/components/PlanCard.js
+++ frontend/src/components/PlanCard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Col, Card, Button } from "react-bootstrap";
-const PlanCard = ({ title, description, color, url }) => {
+const PlanCard = ({ title, description, price, color, url }) => {
   return (
-    <Col>
+    <Col className="d-flex justify-content-center">
       <Card style={{ width: "12rem" }}>
         <i className={"fas fa-star bigStar mx-auto p-3" + color}></i>
         <Card.Body>
           <Card.Title className="text-center">{title}</Card.Title>
+          <Card.Title className="text-center">
+            <strong>${price}</strong> / Month
+          </Card.Title>
           <Card.Text>{description}</Card.Text>
           <Button block variant="secondary" href={url}>
             Select {title}
